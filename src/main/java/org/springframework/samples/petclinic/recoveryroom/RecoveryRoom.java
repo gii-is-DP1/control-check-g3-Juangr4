6f9f402d94743cd46045c1d3fde2d3286908bbdf
Juangr4 <jgarciaruiz2001@gmail.com>
2022-01-14 17:34:24 +0100

    Control Check Terminado con todos los test realizados exitosamente

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoom.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoom.java
@@ -1,17 +1,32 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
-import javax.persistence.Transient;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+
+import org.springframework.lang.NonNull;
+import org.springframework.samples.petclinic.model.NamedEntity;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class RecoveryRoom {
-    Integer id;
-    String name;
-    double size;
+@Entity
+@Table(name = "RecoveryRooms")
+public class RecoveryRoom extends NamedEntity {
+    
+    @NonNull
+    @Min(0)
+    @Column(name = "size")
+    private double size;
+    
+    @Column(name = "secure")
     boolean secure;
-    @Transient
+    
+    @ManyToOne(cascade = CascadeType.ALL, optional = false)
     RecoveryRoomType roomType;
 }
